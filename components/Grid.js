69e1ce0aa49152458af2c730136975c0f002707a
Marion Rose <marionrose@Marions-MacBook-Pro.local>
2022-11-11 14:19:53 -0600

    Added homepage grid hover state

--- components/Grid.js
+++ components/Grid.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import Link from "next/link";
 import imageUrlBuilder from '@sanity/image-url'
 import { client } from "../utils/client";
@@ -13,22 +12,26 @@ export default function Grid({ props }) {
     return (
         <div>
             {props.length > 0 && (
-            <div class="grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+            <div class="relative grid gap-4 grid-cols-2 sm:grid-cols-3 sm:gap-3 md:grid-cols-4 md:gap-4 lg:grid-cols-5 lg:gap-5">
                 {props.map((plant) => (
-                <Link href={`/plants/${plant.slug}`}
-                    key={plant._id}> 
-                        <a 
-                            class="bg-no-repeat bg-cover border-solid border border-black aspect-w-1 aspect-h-1"
-                            style={{
-                                backgroundImage: plant.plantImage
-                                ? `url('${urlFor(plant.plantImage).width(400).height(400).fit("crop")}')`
-                                : undefined,
-                            }}
-                        >   
-                            {/* <p class="hidden hover:flex justify-center items-center text-center text-white font-bold	">{plant?.commonName}</p> */}
-                            <div class="opacity-0 hover:opacity-25 ease-in-out duration-300" style={{backgroundColor: plant.hexCode}} />
-                        </a>
-                </Link>
+                <div key={plant._id} class="border-solid border border-black aspect-w-1 aspect-h-1 flex items-center justify-center z-10 group overflow-hidden">
+                    <Link href={`/plants/${plant.slug}`}> 
+                            <a 
+                                class="bg-no-repeat bg-cover"
+                                style={{
+                                    backgroundImage: plant.plantImage
+                                    ? `url('${urlFor(plant.plantImage).width(400).height(400).fit("crop")}')`
+                                    : undefined,
+                                }}
+                            >   
+                                <div
+                                    style={{backgroundColor: plant.hexCode}} 
+                                    class="shadow-md shadow-black/20 opacity-0 ease-in-out duration-300 group-hover:opacity-100 flex justify-center items-center h-10 absolute -top-10 group-hover:top-0 w-full">
+                                    <div class="text-sm text-white font-medium uppercase mt-2 mb-1">{plant.commonName}</div>
+                                </div>
+                            </a>
+                    </Link>
+                </div>
                 ))}
             </div>
             )}
--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,900;1,100&display=swap"
-          rel="stylesheet"
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;900&display=swap" rel="stylesheet"
         />
         <link rel="icon" href="/plantone-icon.ico" />
 
